a19ade4a3b7f77640469c030d264060eb84d2d7a
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-12-20 11:04:20 +0100

    Magda konsultacje

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe('Login', () => {
+    beforeEach(() => {
+      cy.visit('https://login-test.dfo.no/?idp=idporten')
+    })
+  
+    it('MyID should work', () => {
+    })
+    
+  })
+  
