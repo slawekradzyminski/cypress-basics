f5ea668beba4e517acff18296493a93ae96b73bd
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-12-22 08:46:44 +0100

    work

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -6,6 +6,13 @@ describe('Login', () => {
     })
   
     it('MyID should work', () => {
+      cy.get('#MinIDEksternChain').click()
+      cy.get('#personalIdNumber').type('08089406901')
+      cy.get('#password').type('password01')
+      cy.get('#next').click()
+      cy.get('#otpCode').type('12345')
+      cy.get('#next').click()
+      cy.get('.brandingLogo').should('be.visible')
     })
     
   })
